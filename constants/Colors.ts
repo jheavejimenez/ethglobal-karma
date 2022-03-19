794a57cfa566791b578196a5a2cc5f1024ca5439
JRParreno <jhonrhayparreno22@gmail.com>
2022-03-19 23:51:03 +0800

    added common components

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,6 +1,14 @@
 const tintColorLight = '#2f95dc';
 const tintColorDark = '#fff';
 
+export const DefaultColor = {
+  main: '#2381EE',
+  secondary: '#FFA500',
+  white: '#fff',
+  black: '#000',
+  dark: '#707070',
+}
+
 export default {
   light: {
     text: '#000',
