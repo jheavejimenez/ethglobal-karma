fe1dad0eb2954ee9d9343b5469e2d7d619ab18f0
Jheave <jimenezjheave123@gmail.com>
2022-03-26 17:57:39 +0800

    fix configs

--- lens-hub.ts
+++ lens-hub.ts
@@ -1,11 +1,9 @@
-import { ethers } from 'ethers';
-import { LENS_HUB_ABI, LENS_HUB_CONTRACT } from './config';
 import { getSigner } from './ethers.service';
+import { LENS_HUB_ABI, LENS_HUB_CONTRACT_ADDRESS } from './config';
+import { ethers } from 'ethers';
 
-// lens contract info can all be found on the deployed
-// contract address on polygon.
 export const lensHub = new ethers.Contract(
-  LENS_HUB_CONTRACT,
+  LENS_HUB_CONTRACT_ADDRESS,
   LENS_HUB_ABI,
   getSigner()
-);
+)
