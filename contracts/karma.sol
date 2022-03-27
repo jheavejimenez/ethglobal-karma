722c8d849a27a95c4f1d2b7293fcf70172860521
Jheave <jimenezjheave123@gmail.com>
2022-03-27 22:06:58 +0800

    rename

--- contracts/karma.sol
+++ contracts/karma.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.7;
 import '@openzeppelin/contracts/token/ERC1155/ERC1155.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 
-contract Karma is ERC1155, Ownable {
+contract KarmaPoints is ERC1155, Ownable {
     constructor() ERC1155('') {}
 
     uint256 private KarmaCount = 0;
